5024a1ed332fdcf0e0ca8a90555df57935153d9e
Fernando Vilhena <engesoft@hotmail.com>
2018-07-23 18:43:44 -0300

    First Commit.

---
+++ FooDesignTimeServices.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Microsoft.EntityFrameworkCore.Scaffolding.Internal
+{
+    public class FooDesignTimeServices : IDesignTimeServices
+    {
+        public void ConfigureDesignTimeServices(IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddSingleton<IModelCodeGenerator, FooCSharpModelGenerator>();
+            serviceCollection.AddSingleton<ICSharpEntityTypeGenerator, FooCSharpEntityTypeGenerator>();
+            serviceCollection.AddSingleton<ICSharpDbContextGenerator, FooCSharpDbContextGenerator>();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# Entity Framework 2.1 Scaffold Customization
+When doing the Scaffolding, the EF merge the TableName with SchemaName to create a ClassName. 
+
+*Under Construction yet.*
